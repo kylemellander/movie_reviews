0e23e400fbcd93716174dac477d2aec746a5d2ee
Guest User <student@epicodus.com>
2015-09-23 12:15:44 -0700

    Updated look of admin page

--- app/routes/edit-post.js
+++ app/routes/edit-post.js
@@ -7,7 +7,7 @@ export default Ember.Route.extend({
   actions: {
     update(post, params) {
       Object.keys(params).forEach(function(key) {
-        if(params[key]!==undefined) {
+        if(params[key]!==undefined && params[key]!=="") {
           post.set(key,params[key]);
         }
       });
